b815733be64f4801b5eab432df63d161b2148c20
danylagacione <danielycrys24@gmail.com>
2021-04-08 13:13:41 -0300

    create file products

---
+++ products.py
@@ -0,0 +1,7 @@
+products = {}
+
+products['name'] = str(input("Digite o produto a ser cadastrado :"))
+products['description'] = str(input("Descreva o produto :"))
+products['value'] = float(input("Valor R$ :"))
+
+print (products)
