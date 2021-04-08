731690cd41e3c5bba6c46eb392b127f907a3b2bd
victorhugo <victorblog410@gmail.com>
2021-04-08 13:23:55 -0300

    add function menu category

--- Category.py
+++ Category.py
@@ -1,6 +1,26 @@
-dados = dict()
 
-dados['name'] = str(input('Informe o nome da categoria :'))
-dados['descripino'] = str(input('Informe a descricao da categoria'))
+def menu():
+    print("_______________________________________")
+    print("|                                     |")
+    print("|        Olist Shops - Categoria      |")
+    print("|                                     |")
+    print("|1 - Nova Categoria |")
+    print("|2 - Lista Categoria      |")
+    print("|0 - Sair do Programa                 |")
+    print("|_____________________________________|")
 
-print(dados)
+
+# EXECUÇÃO DO PROGRAMA
+
+
+while True:
+    menu()
+    opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        dados['name'] = str(input('Informe o nome da categoria :'))
+        dados['descripino'] = str(input('Informe a descricao da categoria'))
+    elif opcao == "2":
+        print(dados)
+        print("------------------------------------------------------------------------------")
+    else:
+        print("Opção Inválida!")
